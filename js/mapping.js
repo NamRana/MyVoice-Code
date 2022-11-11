531587cfafd132a4e965f0e42995c6b4b410a388
Naman <sanjeevnamanrana@gmail.com>
2022-11-11 20:22:53 +0530

    Code with voice

---
+++ js/mapping.js
@@ -0,0 +1,14 @@
+
+var wordMAp = new Map(
+    [
+        ["Naman", "Rana"],
+        ["import", "#include < "],
+        ["std", "std"],
+        ["io", "io"],
+        ["dot", "."],
+        ["h", "h"],
+        ["conio", "conio"]
+    ]
+);
+
+console.log("Mapping: ", wordMAp.get("Naman"));
